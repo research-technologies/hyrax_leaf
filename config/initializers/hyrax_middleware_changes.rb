f7aea312a5d3c5f3df831e126c5c59fb1d84742f
Julie Allinson <julie.allinson@london.ac.uk>
2018-11-22 12:47:47 +0000

    change the middleware from to_prepare to Hyrax.config

--- config/initializers/hyrax_middleware_changes.rb
+++ config/initializers/hyrax_middleware_changes.rb
@@ -1,9 +1,7 @@
-Rails.application.configure do
-  config.to_prepare do
+Hyrax.config do | config |
       Hyrax::CurationConcern.actor_factory.swap(Hyrax::Actors::CreateWithFilesActor, 
         Hyrax::Actors::CreateWithFilesOrderedMembersActor)
 
       Hyrax::CurationConcern.actor_factory.swap(Hyrax::Actors::CreateWithRemoteFilesActor, 
         Hyrax::Actors::CreateWithRemoteFilesOrderedMembersActor)
-    end
 end
