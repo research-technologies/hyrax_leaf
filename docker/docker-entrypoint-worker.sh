51ed18668dbd53e707cfc1fd5ebbecdfde64c572
Rory McNicholl <rory.mcnicholl@london.ac.uk>
2021-10-04 10:19:47 +0000

    keep pod running if sidekiq fails...

--- docker/docker-entrypoint-worker.sh
+++ docker/docker-entrypoint-worker.sh
@@ -4,4 +4,8 @@
 bash /bin/docker-entrypoint.sh
 
 echo "--------- Starting Sidekiq in $RAILS_ENV mode ---------"
-bundle exec sidekiq
+SIDEKIQ_START=`bundle exec sidekiq`
+if [ "$?" -ne "0" ]; then
+  echo "### There was an issue starting sidekiq. We have kept this container alive for you to go and see what's up ###"
+  tail -f /dev/null
+fi
